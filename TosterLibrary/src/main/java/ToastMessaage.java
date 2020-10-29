da730902b729c8b3509a843c81628a281ba42661
Md. Amirul Islam <amirul@mislbd.com>
2020-10-29 11:48:51 +0600

    go next acitivity

--- TosterLibrary/src/main/java/ToastMessaage.java
+++ TosterLibrary/src/main/java/ToastMessaage.java
@@ -1,9 +1,16 @@
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.Toast;
 
 public class ToastMessaage {
 
     public static void s(Context c, String message){
         Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+    }
+
+    public static void goToNextActivity(Context context, final Class<? extends Activity> targetActivity){
+        Intent in = new Intent(context, targetActivity);
+        context.startActivity(in);
     }
 }
