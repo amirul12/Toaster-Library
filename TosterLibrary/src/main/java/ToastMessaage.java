2a430556ebc48a5ce455230ce92eb9a66b4f8b0c
Md. Amirul Islam <amirul@mislbd.com>
2020-10-29 11:32:21 +0600

    initial commit

---
+++ TosterLibrary/src/main/java/ToastMessaage.java
@@ -0,0 +1,9 @@
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToastMessaage {
+
+    public static void s(Context c, String message){
+        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+    }
+}
